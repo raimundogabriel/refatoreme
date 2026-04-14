c778fdb1c7ec0506432737b21f5e66830dbef1ea
Gabriel Raimundo <raimundogabriel18@gmail.com>
2026-04-14 15:37:55 -0300

    feat: api da home

--- meu-novo-app/src/pages/home/Home.jsx
+++ meu-novo-app/src/pages/home/Home.jsx
@@ -1,8 +1,41 @@
 import './Home.css'
 import Banner from '../../components/principal/Banner';
 import Card from '../../components/principal/Card';
+import React, { useState, useEffect } from 'react';
 
-function Home(){
+
+function Home() {
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const fetchCharacters = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch('https://swapi.dev/api/people');
+
+            if (!response.ok) {
+                throw new Error('Erro ao buscar os dados');
+            }
+
+            const data = await response.json();
+            setCharacters(data.results); // O array de personagens fica em 'results'
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    // Dispara o fetch quando o componente renderiza pela primeira vez
+    useEffect(() => {
+        fetchCharacters();
+    }, []);
+
+    if (loading) return <p>Carregando personagens...</p>;
+    if (error) return <p>Erro: {error}</p>;
+
+
     const servicesData = [
         {
             id: 1,
@@ -25,12 +58,12 @@ function Home(){
             description: "Estruturação de dashboards em Power BI e modelagem de dados para tomada de decisão."
         },
         {
-            id:5,
+            id: 5,
             title: "Inteligência Artificial",
             description: "Projetos focados em aprendizado de maquina e LLMs."
         },
         {
-            id:6,
+            id: 6,
             title: "Gerênciamento de Projetos",
             description: "Consultoria especializada em análise, desenvolvimento e acompanhamento de resultados de projetos."
         }
@@ -38,7 +71,7 @@ function Home(){
     return (
         <main>
             <Banner />
-
+            
             <section id="services" className="cards-section">
                 <h2>Nossas Especialidades</h2>
                 <div id="cards-container" className="cards-grid">
@@ -51,6 +84,14 @@ function Home(){
                     ))}
                 </div>
             </section>
+            <div>
+                <h1>Personagens de Star Wars</h1>
+                <ul>
+                    {characters.map((person, index) => (
+                        <li key={index}>{person.name}</li>
+                    ))}
+                </ul>
+            </div>
         </main>
     )
 }
